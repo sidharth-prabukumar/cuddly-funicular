b2fb0650b68859c02b19cb7ba33335b1ce168069
sidharth-prabukumar <sidharth.prabukumar@gmail.com>
2019-06-22 01:16:10 -0500

    Initial commit

--- OpenGLProj_1/main.cpp
+++ OpenGLProj_1/main.cpp
@@ -1,9 +1,25 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <thread>
 
 int main()
 {
 	glfwInit();
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	glfwTerminate();
+	return 0;
 }
