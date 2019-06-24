a733b77fba654ff19c282a1cd70b78f24b60b379
sidharth-prabukumar <sidharth.prabukumar@gmail.com>
2019-06-23 20:21:02 -0500

    Uniforms with vsync

--- OpenGLStuff/Application.cpp
+++ OpenGLStuff/Application.cpp
@@ -109,6 +109,8 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	glfwSwapInterval(1);
+
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "Error!" << std::endl;
@@ -149,13 +151,25 @@ int main(void)
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
+	int location = glGetUniformLocation(shader, "u_Color");
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.6f, 0.3f, 0.8f, 1.0f));
+	float r = 0.0f;
+	float increment = 0.05f;
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+		r += increment;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
--- OpenGLStuff/Basic.shader
+++ OpenGLStuff/Basic.shader
@@ -11,7 +11,9 @@ gl_Position = position;
 #version 410 core
 
 layout(location = 0) out vec4 color;
+
+uniform vec4 u_Color;
 void main()
 {
-color = vec4(0.2, 0.3, 0.8, 1.0);
+	color = u_Color;
 };
