38ba0fdf9f6e6688cf721c5f43b3fbb7dea6706b
sidharth-prabukumar <sidharth.prabukumar@gmail.com>
2019-07-13 18:13:27 -0500

    Experimented with projection matrices

--- OpenGLStuff/Application.cpp
+++ OpenGLStuff/Application.cpp
@@ -30,7 +30,7 @@ int main(void)
 
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(800, 600, "Application", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Application", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -49,10 +49,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	float positions[] = {
-		-0.5f, -0.5f, 0.0f, 0.0f,	//0
-		 0.5f, -0.5f, 1.0f, 0.0f,	//1
-		 0.5f,  0.5f, 1.0f, 1.0f,	//2
-		-0.5f,  0.5f, 0.0f, 1.0f,	//3
+		 100.0f, 100.0f, 0.0f, 0.0f,	//0
+		 200.0f, 100.0f, 1.0f, 0.0f,	//1
+		 200.0f, 200.0f, 1.0f, 1.0f,	//2
+		 100.0f, 200.0f, 0.0f, 1.0f,	//3
 	};
 
 	unsigned int indices[] = {
@@ -73,7 +73,10 @@ int main(void)
 
 	IndexBuffer ib(indices, 6);
 
-	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+	glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+
+	glm::vec4 result = proj * vp;
 
 	Shader shader("Basic.shader");
 	shader.Bind();
