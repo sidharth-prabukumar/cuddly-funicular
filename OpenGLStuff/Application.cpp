4cf7fc84c2ffb005c44d4a741493f54906dbf4b5
sidharth-prabukumar <sidharth.prabukumar@gmail.com>
2019-07-13 19:06:53 -0500

    Model View Projection

--- OpenGLStuff/Application.cpp
+++ OpenGLStuff/Application.cpp
@@ -74,14 +74,16 @@ int main(void)
 	IndexBuffer ib(indices, 6);
 
 	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));	//Creating an identity view matrix and Translating the matrix to simi=ulate a camera moving to the right,
+																				//which means that the object is moving to the left
+	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-	glm::vec4 result = proj * vp;
+	glm::mat4 mvp = proj * view * model;	//Read matrix multplication from right to left -> model x view x proj
 
 	Shader shader("Basic.shader");
 	shader.Bind();
 	shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-	shader.SetUniformMat4f("u_MVP", proj);
+	shader.SetUniformMat4f("u_MVP", mvp);
 
 	Texture texture("res/textures/horn.png");
 	texture.Bind();
